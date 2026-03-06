0f5d5232a5e21bdcdeb81ba3aeb389c8e643bb49
Laxmikant <laxmikantmaity1944@gmail.com>
2026-03-06 11:55:44 +0530

    Update install.sh

--- terminal/install.sh
+++ terminal/install.sh
@@ -42,39 +42,54 @@ sleep 1
 ## Installing Fonts
 
 figlet "Installing Fonts" | sed 's/^/\x1b[36m/' ; echo -e "\x1b[0m"
+echo "Do you want to change the Bootloader theme, Sir ?? [Y/n]: "
+read fontans
 
-sleep 3
-
-
-cd ~/laxmikant-terminal/fonts/
-sleep 1
-sudo cp -r * /usr/share/fonts
-sudo fc-cache -fv
-sleep 2
+if [[ $fontans == "y" || $fontans == "Y" ]]; then
+  cd ~/laxmikant-terminal/fonts/
+  sleep 1
+  sudo cp -r * /usr/share/fonts
+  sudo fc-cache -fv
+  sleep 2
+  cd ~
+else
+  echo "Skipping font installation..."
+fi
 
 ## Theming the bootloader
-
+cd ~
 figlet "Themeing Bootloader" | sed 's/^/\x1b[36m/' ; echo -e "\x1b[0m"
 
-git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
-cd Top-5-Bootloader-Themes
-sudo ./install.sh
 
-sleep 5
 
-sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+echo "Do you want to change the Bootloader theme, Sir ?? [Y/n]: "
+read grubans
 
-## Installing the DE 
-
-figlet "Installing GNOME DE" | sed 's/^/\x1b[36m/' ; echo -e "\x1b[0m"
-
-sudo dnf group install gnome-desktop
-sudo systemctl set-default graphical.target
-sudo systemctl enable gdm
+if [[ $grubans == "y" || $grubans == "Y" ]]; then
+  cd ~
+  git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
+  cd Top-5-Bootloader-Themes
+  sudo ./install.sh
+  sleep 5
+  sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+  sleep 2
+  cd ~
+else
+  echo "Okkayy, skipping the Bootloader configuration :-)..."
+fi
 
 ## Rebooting the System
 
 figlet "Rebooting" | sed 's/^/\x1b[36m/' ; echo -e "\x1b[0m"
 
-sleep 10
-sudo reboot
+#Permission
+echo "Would you like the system to reboot, Sir ?? [Y/n]:"
+read rebans
+
+if [[ $rebans == "y" || $rebans == "y" ]]; then
+  echo "Noted Sir, the system will reboot in 5 seconds..."
+  sleep 5
+  sudo reboot
+else
+  echo "Got it, falling back to prompt..."
+fi
