e40303b3e627533f8ed826c4abd94175d0b3b7fa
Laxmikant <laxmikantmaity1944@gmail.com>
2026-04-15 12:03:37 +0530

    Create terminal.sh

---
+++ scripts/terminal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#===============================
+# Terminal Setup script !!
+#===============================
+
+sudo apt install -y fish fastfetch
+sleep 1
+curl -sS https://starship.rs/install.sh | sh
+sleep 1
+
+mkdir -p ~/.config
+cp -r ~/fancy-desktop/assets/dotfiles/config/* ~/.config/
+chsh -s /usr/bin/fish
+
+
+sudo mkdir -p /root/.config/
+sudo cp -r ~/fancy-desktop/assets/dotfiles/config/* /root/.config/
+sudo chsh -s /usr/bin/fish
+
+echo "Your Terminal is ready"
