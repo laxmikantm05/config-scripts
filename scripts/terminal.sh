5a8a2aecd2a609c1c68326a9f5399bf40ef7fadd
Laxmikant <laxmikantmaity1944@gmail.com>
2026-04-15 12:11:23 +0530

    Update terminal.sh

--- scripts/terminal.sh
+++ scripts/terminal.sh
@@ -9,12 +9,12 @@ curl -sS https://starship.rs/install.sh | sh
 sleep 1
 
 mkdir -p ~/.config
-cp -r ~/fancy-desktop/assets/dotfiles/config/* ~/.config/
+cp -r ~/fancy-desktop/assets/dotfiles/.config/* ~/.config/
 chsh -s /usr/bin/fish
 
 
 sudo mkdir -p /root/.config/
-sudo cp -r ~/fancy-desktop/assets/dotfiles/config/* /root/.config/
+sudo cp -r ~/fancy-desktop/assets/dotfiles/.config/* /root/.config/
 sudo chsh -s /usr/bin/fish
 
 echo "Your Terminal is ready"
